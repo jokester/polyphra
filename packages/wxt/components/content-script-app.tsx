5e705db348b50e69a025145a284287374df9b964
Wang Guan <momocraft@gmail.com>
2025-06-26 23:08:28 +0900

    wip

--- packages/wxt/components/content-script-app.tsx
+++ packages/wxt/components/content-script-app.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import {createRoot} from 'react-dom/client'
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import { Button } from "primereact/button";
+
+
 const App: React.FC = ( ) => {
-    return <div onClick={() => alert('hi')}>fff</div>
+    return <Button onClick={() => alert('hi')}>fff</Button>
 }
 
-export function mount(el: HTMLDivElement) {
+export function mount(el: HTMLElement) {
     const r = createRoot(el)
-    r.render(<App />)
+    r.render(<PrimeReactProvider><App /></PrimeReactProvider>)
 }
--- packages/wxt/entrypoints/content.tsx
+++ packages/wxt/entrypoints/content.tsx
@@ -1,14 +1,27 @@
-
-import {mount} from '../components/content-script-app'
+import { ContentScriptContext } from 'wxt/dist/utils/content-script-context';
+import { mount } from '../components/content-script-app';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
 export default defineContentScript({
-  matches: ["*://*/*"],
+  matches: ['<all_urls>'],
+  cssInjectionMode: 'ui',
 
-  main(ctx: ContentScriptContext) {
+  async main(ctx: ContentScriptContext) {
     // Executed when content script is loaded, can be async
-    console.info("content script running")
+    console.info('content script running');
 
-    const el = document.createElement('div')
-    document.body.prepend(el)
-    mount(el)
+    const ui = await createShadowRootUi(ctx, {
+      name: 'polyphra-content-script-ui',
+      position: 'inline',
+      anchor: 'body',
+      onMount(container: HTMLElement) {
+        // Define how your UI will be mounted inside the container
+        // const app = document.createElement('p');
+        // app.textContent = 'Hello world!';
+        // container.append(app);
+        mount(container);
+      },
+    });
+
+    ui.mount();
   },
 });
